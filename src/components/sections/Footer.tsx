51b937941e8beaa676c40b43a450bb848cc5d335
saadmusani <saadmusani@Saads-MacBook-Air.local>
2026-05-25 21:03:12 +0530

    Remove hardcoded date from footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -53,7 +53,7 @@ export function Footer() {
         </div>
 
         <div className="flex flex-col gap-2 border-t border-white/5 pt-6 font-mono text-[10px] uppercase tracking-[0.28em] text-zinc-500 md:flex-row md:items-center md:justify-between">
-          <span>Build 2026.04.21 &nbsp;&middot;&nbsp; Mark LXXXV &nbsp;&middot;&nbsp; J.A.R.V.I.S. Online</span>
+          <span>Mark LXXXV &nbsp;&middot;&nbsp; J.A.R.V.I.S. Online</span>
           <span>Proof of concept &mdash; fan art, no commercial use</span>
         </div>
       </div>
